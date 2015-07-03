d2996789d1eb3954640c9c323daaf448cbc9eaa0
Scott Frees <scott.frees@gmail.com>
2015-07-03 13:40:46 -0400

    threejs example resources

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,21 +3,33 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
   prompting: function () {
     var done = this.async();
-    this.prompt({
-      type    : 'input',
-      name    : 'name',
-      message : 'Your project name',
-      default : this.appname // Default to current folder name
-    }, function (answers) {
+    this.prompt([
+      {
+        type    : 'input',
+        name    : 'name',
+        message : 'Your project name',
+        default : this.appname // Default to current folder name
+      }, 
+      {
+        type : "confirm", 
+        name : "three_textures", 
+        message : "Do you want to include standard textures (from http://threejs.org/)?", 
+        default : true
+      }, 
+      {
+        type : "confirm", 
+        name : "three_utils", 
+        message : "Do you want to include standard threejs utilities (from http://threejs.org/)?", 
+        default : true
+      }
+
+
+    ], function (answers) {
       this.appname = answers.name;
+      this.three_textures = answers.three_textures;
+      this.three_utils = answers.three_utils;
       done();
     }.bind(this));
-  },
-  setup_view: function () {
-    console.log('view setup just ran');
-  },
-  setup_trackers: function () {
-    console.log('trackr setup just ran');
   }, 
   writing: function () {
     var opts = { title : this.appname }
@@ -36,6 +48,8 @@ module.exports = generators.Base.extend({
       this.destinationPath(this.appname + '/package.json'),
       opts
     );
+    if (this.three_textures) this.directory("three/textures", this.appname + "/three/textures");
+    if (this.three_utils) this.directory("three/js", this.appname + "/three/js");
   }, 
   post_scaffold_message: function() {
   	console.log('vrjs application created at ' + this.destinationPath(this.appname));
